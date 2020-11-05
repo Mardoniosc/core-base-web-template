aabca1e4954fd5110d1103ef823b70374873c33e
Mardoniosc <mardonio@live.com>
2020-11-05 18:51:20 -0300

    Criado os modulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,45 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'dashboard', pathMatch: 'full'
+  },
+  {
+    path: 'users',
+    loadChildren: () =>
+      import('./pages/users/users.module').then((m) => m.UsersModule),
+  },
+  {
+    path: 'profiles',
+    loadChildren: () =>
+      import('./pages/profiles/profiles.module').then((m) => m.ProfilesModule),
+  },
+  {
+    path: 'permitions',
+    loadChildren: () =>
+      import('./pages/permitions/permitions.module').then(
+        (m) => m.PermitionsModule
+      ),
+  },
+  {
+    path: 'historys',
+    loadChildren: () =>
+      import('./pages/historico-log/historico-log.module').then(
+        (m) => m.HistoricoLogModule
+      ),
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./pages/dashboard/dashboard.module').then(
+        (m) => m.DashboardModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/pages/historico-log/historico-log.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HistoricoLogRoutingModule } from './historico-log-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    HistoricoLogRoutingModule
+  ]
+})
+export class HistoricoLogModule { }
