f4a7d50f5b17982f92330bd82cf729b47b015a96
Mardoniosc <mardonio@live.com>
2020-11-05 23:28:20 -0300

    Adicionado layout do profile

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-import { BreadCrumbComponent, FormFieldErrorComponent } from './components';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [BreadCrumbComponent, FormFieldErrorComponent],
+  declarations: [],
   imports: [CommonModule, ReactiveFormsModule, RouterModule],
   exports: [
     // Módulos
@@ -13,8 +12,6 @@ import { RouterModule } from '@angular/router';
     ReactiveFormsModule,
 
     // Components
-    BreadCrumbComponent,
-    FormFieldErrorComponent,
   ],
 })
 export class SharedModule {}
