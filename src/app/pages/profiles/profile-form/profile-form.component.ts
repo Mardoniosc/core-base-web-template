ba9b0445318685143df2a22a498ffee8b634ad51
programadorsemcortes <programadorsemcortes@gmail.com>
2020-11-06 09:40:43 -0300

    Utilizando o componente breadcrumb

--- src/app/pages/profiles/profile-form/profile-form.component.ts
+++ src/app/pages/profiles/profile-form/profile-form.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injector } from '@angular/core';
+import { Validators } from '@angular/forms';
+import { BaseResourceFormComponent } from 'src/app/shared/components';
+import { Profile, ProfileService } from '../shared/';
 
 @Component({
   selector: 'app-profile-form',
   templateUrl: './profile-form.component.html',
-  styleUrls: ['./profile-form.component.css']
+  styleUrls: ['./profile-form.component.css'],
 })
-export class ProfileFormComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class ProfileFormComponent extends BaseResourceFormComponent<Profile> {
+  constructor(
+    protected profileService: ProfileService,
+    protected injector: Injector
+  ) {
+    super(injector, new Profile(), profileService, Profile.fromJson);
   }
 
+  protected buildResourceForm() {
+    this.resourceForm = this.formBuilder.group({
+      id: [null],
+      nome: [null, [Validators.required, Validators.minLength(2)]],
+    });
+  }
+
+  protected createPageTitle(): string {
+    return 'Cadastro de Novo Perfil';
+  }
+
+  protected editionPageTitle(): string {
+    const categoryName = this.resource.nome || '';
+    return 'Editando Perfil: ' + categoryName;
+  }
 }
